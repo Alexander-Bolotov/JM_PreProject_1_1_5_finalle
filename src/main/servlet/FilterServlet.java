284b0dec6797f2994ce4e245d262aad20b1f4b3f
Alexander-Bolotov <37707228+Alexander-Bolotov@users.noreply.github.com>
2020-05-26 00:38:14 +0400

    Initial commit

---
+++ src/main/servlet/FilterServlet.java
@@ -0,0 +1,53 @@
+package src.main.servlet;
+
+import src.main.model.User;
+import src.main.service.Service;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+//@WebServlet("/filter")
+@WebFilter("/filter")
+public class FilterServlet implements Filter {
+    private FilterConfig filterConfig = null;
+    private boolean active = false;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+        String act = filterConfig.getInitParameter("active");
+        if (act != null) {
+            active = (act.toUpperCase().equals("TRUE"));
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        Service service = Service.getInstance();
+
+        String name = httpServletRequest.getParameter("name");
+        String password = httpServletRequest.getParameter("password");
+        User user = new User(name, password);
+
+        String role = service.getRoleByName(name);
+        if (name.isEmpty()|| password.isEmpty()){
+            System.out.println("Не введен логин или пароль");
+        } else if (!service.nameIsExist(name)){
+            System.out.println("Такого логина не существует");
+        } else if (role.equals("Admin")) {
+            servletRequest.getRequestDispatcher("admin.jsp").forward(servletRequest, servletResponse);
+        } else if(role.equals("User")){
+            servletRequest.getRequestDispatcher("user.jsp").forward(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        filterConfig = null;
+    }
+}
---
+++ src/main/servlet/RegistrationServlet.java
@@ -0,0 +1,36 @@
+package src.main.servlet;
+
+
+import src.main.model.User;
+import src.main.service.Service;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/registration")
+public class RegistrationServlet extends HttpServlet {
+    private Service service = Service.getInstance();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        String name = req.getParameter("name");
+        String password = req.getParameter("password");
+        String role = req.getParameter("role");
+        if (name == null || password == null) {
+            resp.setContentType("text/html;charset=utf-8");
+            resp.getWriter().println("Не введен пароль или логин");
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        } else if(service.addUser(new User(name, password, role))) {
+            req.getRequestDispatcher("admin.jsp").forward(req, resp);
+        } else {
+            resp.setContentType("text/html;charset=utf-8");
+            resp.getWriter().println("Какая-то фигня");
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+}
